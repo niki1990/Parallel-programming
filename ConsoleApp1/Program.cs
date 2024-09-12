a4d0eaedc67eebbcb6e62ba04aa57c49e2c08516
niki1990 <mnilesh50@gmail.com>
2024-09-12 23:38:36 +0530

    List operation using Task.WhenAll

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,38 @@
 ﻿
 
-Console.WriteLine("Hello from Main!");
+using ConsoleApp1;
+
+List<string> list = new List<string>();
+List<string> masklist = new List<string>();
+Random random = new Random();
+for (int g = 0; g <= 100000; g++)
+{
+    string randomNumber = string.Join(string.Empty, Enumerable.Range(0, 10).Select(number => random.Next(0, 9).ToString()));
+    list.Add(randomNumber);
+}
+
+await Task.WhenAll(list.Select(async x => masklist.Add(x.Substring(0, 5) + "****")));
+
+
+//Parallel.ForEach(list, str =>
+//{
+//    masklist.Add(str.Substring(0,5)+"****");
+//});
+Thread.Sleep(10000);
+
+/////////////////////////////////////////////
 Task t = Task.Run(() => {
     
     Console.WriteLine("This is First Task");
 });
 t.Wait();
 
+
 //Checked the Task status 
 if(t.Status==TaskStatus.RanToCompletion)
 Console.WriteLine("Finished");
 
+////////////////////////////////////////////////
 //Continuations allow tasks to chain together, meaning one task can start once another completes.
 
 Task firstTask = Task.Run(() => {
@@ -23,6 +45,8 @@ Task SecondTaslk = firstTask.ContinueWith(t => Console.WriteLine("This is second
 
 SecondTaslk.Wait();
 
+
+////////////////////////////////////////////////////
 //The Parallel class simplifies running loops in parallel. 
 //it runs multiple iterations at the same time.
 
@@ -31,6 +55,8 @@ Console.WriteLine($"{i}");
 });
 Console.WriteLine("-------------------");
 
+
+///////////////////////////////////////////////////
 //Processing Collections with Parallel.ForEach
 
 List<int> ListOfInt = new List<int> { 1, 2, 3, 4, 5, 6 };
@@ -41,7 +67,7 @@ Parallel.ForEach(ListOfInt, number =>
 });
 
 
-
+//////////////////////////////////////////////////
 //Exception Handling in Parallel Loops
 try
 
@@ -69,4 +95,19 @@ catch (AggregateException ae)
     }
 }
 
+//////////////////////////////////////////////////////////////////
+//Async / Await in Parallel Programming
+TaskClass taskClass = new TaskClass();
+await taskClass.GetResult();
+Console.WriteLine("GetResult runned..");
 
+/////////////////////////////////////////////////////////////////
+/////PLINQ stands for Parallel LINQ, which allows for parallel querying of data.
+///
+var data = Enumerable.Range(1, 100).ToList();
+
+var parallelQuery = data.AsParallel().Where(x => x % 2 == 0).ToList();
+
+
+
+parallelQuery.ForEach(Console.WriteLine);
---
+++ ConsoleApp1/TaskClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TaskClass
+    {
+       public  async Task GetResult()
+        {
+            await Task.Run(() => {
+            Thread.Sleep(2000);
+            });
+            Console.WriteLine("GetResult executed...");
+        }
+    }
+}
