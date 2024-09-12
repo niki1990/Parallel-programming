8dc7da8bff616c7b19b2f83aa761b0faf8018df4
niki1990 <mnilesh50@gmail.com>
2024-09-12 12:57:55 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Hello from Main!");
+Task t = Task.Run(() => {
+    
+    Console.WriteLine("This is First Task");
+});
+t.Wait();
+if(t.Status==TaskStatus.RanToCompletion)
+Console.WriteLine("Finished");
